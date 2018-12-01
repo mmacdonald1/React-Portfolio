8cf8a9f6610aa3f76acce153f27415a938e4be10
Melanie MacDonald <melmacd94@gmail.com>
2018-11-30 20:06:18 -0500

    components

---
+++ src/containers/PortfolioContainer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Project from '../components/Project'
+
+const PortfolioContainer = (props) => {
+  return(
+    <div>
+      PortfolioContainer
+      <Project />
+   </div>
+  )
+}
+
+export default PortfolioContainer
