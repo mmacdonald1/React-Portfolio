f39ef054c2725e1ab0ab30b85842015d4ee3b79e
mmacdonald1 <melmacd94@gmail.com>
2019-01-08 09:32:13 -0500

    project-cards styled

--- src/components/Project.js
+++ src/components/Project.js
@@ -7,7 +7,7 @@ const Project = (props) => {
       <h3>{props.project.name}</h3>
       <h4>{props.project.technologies}</h4>
       <p>{props.project.description}</p>
-      <Button href={props.project.url} bsStyle="primary">Live Demo</Button>
+      <Button className="first-button" href={props.project.url} bsStyle="primary">Live Demo</Button>
       <Button href={props.project.githubRepo} bsStyle="primary">Github Repo</Button>
     </Well>
   )
