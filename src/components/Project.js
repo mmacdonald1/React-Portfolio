ab52e70f23efc03939eb5dbbe8c52de5a932709b
mmacdonald1 <melmacd94@gmail.com>
2019-01-10 19:31:33 -0500

    open in new page

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,16 +1,25 @@
-import React from 'react'
+import React,{Component} from 'react'
 import {Well, Button} from 'react-bootstrap'
 
-const Project = (props) => {
-  return(
-    <Well className="project-well">
-      <h3>{props.project.name}</h3>
-      <h4>{props.project.technologies}</h4>
-      <p>{props.project.description}</p>
-      <Button className="first-button" href={props.project.url} bsStyle="primary">Live Demo</Button>
-      <Button href={props.project.githubRepo} bsStyle="primary">Github Repo</Button>
-    </Well>
-  )
+class Project extends Component{
+  handleUrl=()=>{
+    window.open( this.props.project.url, "_blank")
+  }
+  handleGit=()=>{
+    window.open( this.props.project.githubRepo, "_blank")
+  }
+
+  render(){
+    return(
+      <Well className="project-well">
+        <h3>{this.props.project.name}</h3>
+        <h4>{this.props.project.technologies}</h4>
+        <p>{this.props.project.description}</p>
+        <Button className="first-button" onClick={this.handleUrl} bsStyle="primary">Live Demo</Button>
+        <Button onClick={this.handleGit} bsStyle="primary">Github Repo</Button>
+      </Well>
+    )
+  }
 }
 
 export default Project
--- src/containers/PortfolioContainer.js
+++ src/containers/PortfolioContainer.js
@@ -3,12 +3,13 @@ import Project from '../components/Project'
 
 class PortfolioContainer extends Component{
   state={
-    projects: [{name:"Hustler", technologies:"React, Redux, Redux Thunk, React Router, Rails, Postgresql", description:"A notecard creating and quizzing app", url:"", githubRepo:"https://github.com/mmacdonald1/hustler-frontend"},{name:"Github API Project", technologies:"React, ReactStrap", description:"A simple UI to find Github profile information", url:"https://mmacdonald1.github.io/Github-API-Project/", githubRepo:"https://github.com/mmacdonald1/Github-API-Project"}, {name:"Leg Work", technologies:"MySQL, Sequelize, Passport, Handlebars, Express, Node, JavaScript, Bootstrap", description:"An application manager to help ease the job search process", url:"https://legwork.herokuapp.com/", githubRepo:"https://github.com/mmacdonald1/Leg-Work"}]
+    projects: [{name:"Hustler", technologies:"React, Redux, Redux Thunk, React Router, Rails, Postgresql", description:"A notecard creating and quizzing app", url:"https://github.com/mmacdonald1/hustler-frontend", githubRepo:"https://github.com/mmacdonald1/hustler-frontend"},{name:"Github API Project", technologies:"React, ReactStrap", description:"A simple UI to find Github profile information", url:"https://mmacdonald1.github.io/Github-API-Project/", githubRepo:"https://github.com/mmacdonald1/Github-API-Project"}, {name:"Leg Work", technologies:"MySQL, Sequelize, Passport, Handlebars, Express, Node, JavaScript, Bootstrap", description:"An application manager to help ease the job search process", url:"https://legwork.herokuapp.com/", githubRepo:"https://github.com/mmacdonald1/Leg-Work"}]
   }
   render(){
     return(
       <div id="projects">
         <h1>My Work</h1>
+        <p> Just a heads up! Projects hosted on heroku take longer to load. </p>
         <div className="project-container">
           {this.state.projects.map((project, index) => <Project key={index} project={project}/>)}
         </div>
