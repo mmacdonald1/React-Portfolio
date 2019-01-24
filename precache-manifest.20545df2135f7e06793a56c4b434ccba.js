7da9471d9be3923693b3fb2b132ccbe72293a5e4
mmacdonald1 <melmacd94@gmail.com>
2019-01-23 19:45:25 -0500

    Updates

---
+++ precache-manifest.20545df2135f7e06793a56c4b434ccba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f546dc0613722202b0a1",
+    "url": "/static/js/main.f546dc06.chunk.js"
+  },
+  {
+    "revision": "9327bedf6246b01ac36e",
+    "url": "/static/js/1.9327bedf.chunk.js"
+  },
+  {
+    "revision": "f546dc0613722202b0a1",
+    "url": "/static/css/main.136d09dd.chunk.css"
+  },
+  {
+    "revision": "01b45a8faa5bc53c2a2eb0dec1cc5bc9",
+    "url": "/index.html"
+  }
+];
